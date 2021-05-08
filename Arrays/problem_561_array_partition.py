1fcff5192dee17d39f38fb1499850a4176bf0bf7
vigneshthiagarajan <vigneshthiagaraj@gmail.com>
2021-05-07 20:37:20 -0500

    Changes

--- Arrays/problem_561_array_partition.py
+++ Arrays/problem_561_array_partition.py
@@ -1,4 +1,5 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
+        # Alternate sum of smallest elements, after sorting
         max_sum = sum(sorted(nums)[::2])
         return max_sum
