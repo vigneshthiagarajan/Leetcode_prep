8cc4440f8b2a2d753fd71e1b3a5e6e6df26495d9
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-05-25 12:20:06 -0500

    Adding solution.

--- Arrays/problem_977. Squares of a Sorted Array.py
+++ Arrays/problem_977. Squares of a Sorted Array.py
@@ -5,4 +5,27 @@ class Solution:
         squared.sort()
         return squared
 
+#         # Two pointer
+#         negatives = [i for i in nums if i<0]
+#         positives = [i for i in nums if i>=0]
 
+#         negative_squares = [i*i for i in negatives]
+#         positive_squares = [i*i for i in positives]
+#         pointer_1 = len(negative_squares)-1
+#         pointer_2 = 0
+#         all_squares = []
+#         while(pointer_1>=0 and pointer_2<len(positive_squares)):
+#             if(negative_squares[pointer_1]<=positive_squares[pointer_2]):
+#                 all_squares.append(negative_squares[pointer_1])
+#                 pointer_1-=1
+#             else:
+#                 all_squares.append(positive_squares[pointer_2])
+#                 pointer_2+=1
+#         if(pointer_1 >= 0):
+#             print(negative_squares[pointer_1::-1])
+#             all_squares = all_squares + negative_squares[pointer_1::-1]
+#         elif(pointer_2<len(positive_squares)):
+#             all_squares = all_squares + positive_squares[pointer_2::1]
+
+
+#        return all_squares
