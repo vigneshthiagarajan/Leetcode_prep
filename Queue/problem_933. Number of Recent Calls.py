94748688c7d437824cba629e441aa0313ff526e3
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-05-26 16:04:28 -0500

    Adding solution.

---
+++ Queue/problem_933. Number of Recent Calls.py
@@ -0,0 +1,19 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.request_times = deque()
+
+    def ping(self, t: int) -> int:
+        # Add to queue
+        self.request_times.append(t)
+
+        # Delete unnecessary elements from left (Since we always have increasing order of request
+        # times
+        while t - self.request_times[0] > 3000:
+            self.request_times.popleft()
+
+        return len(self.request_times)
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
