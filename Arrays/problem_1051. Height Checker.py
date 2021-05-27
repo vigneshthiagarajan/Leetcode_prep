103e16be39f8d4f9bd7e4a6d698fa24619c61669
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-05-27 00:11:56 -0500

    Adding solution.

---
+++ Arrays/problem_1051. Height Checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        num_indices_mismatch = 0
+        expected_heights = sorted(heights)
+        for i in range(len(heights)):
+            if (heights[i] != expected_heights[i]):
+                num_indices_mismatch += 1
+        return num_indices_mismatch
