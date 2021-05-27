f8654694a06bb2bd4750f58df0fb173d15033bb1
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-05-27 14:25:57 -0500

    Adding solution.

--- Strings/problem_1736. Latest Time by Replacing Hidden Digits.py
+++ Strings/problem_1736. Latest Time by Replacing Hidden Digits.py
@@ -29,27 +29,5 @@ class Solution:
 
         return time
 
-    # # if hour 10's aand Units value is "?"
-    #     if time[0]=="?" and time[1]=="?":
-    #                 time = time.replace("?", "2", 1)
-    #                 time = time.replace("?", "3", 1)
-    #     # If hour units value greater than 3, hour tens value will be 1 else 2
-    #     if time[0]=="?" :
-    #             if time[1]>"3":
-    #                     time=time.replace("?","1",1)
-    #             else:
-    #                     time = time.replace("?", "2", 1)
-    #     # if hour 10's value is 0 or 1
-    #     if time[0]=="1" or time[0]=="0":
-    #             if time[1]=="?":
-    #                     time = time.replace("?", "9", 1)
-    #     # if hour 10's value is 2
-    #     if time[0] == "2":
-    #             if time[1] == "?":
-    #                     time = time.replace("?", "3", 1)
-    #     # if minute 10's and units value is ?
-    #     if time[3]=="?":
-    #             time = time.replace("?", "5", 1)
-    #     if time[4]=="?":
-    #             time = time.replace("?", "9", 1)
-    #     return(time)
+
+
