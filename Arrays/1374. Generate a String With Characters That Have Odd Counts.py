db68dfda788310aab681b458fb68945e2def188c
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-01 23:29:06 -0500

    Adding solution.

---
+++ Arrays/1374. Generate a String With Characters That Have Odd Counts.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        filler = ['a', 'b', 'c']
+        if(n == 1):
+            return filler[0]
+        if(n%2 == 0):
+            result = filler[0]*(n-1) + filler[1]
+        else:
+            result = filler[0]*(n-2) + filler[1] + filler[2]
+        return result
