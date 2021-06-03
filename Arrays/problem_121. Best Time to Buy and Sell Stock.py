b6111c6552aeb5f41d73a47510e12b7b58b7ef81
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-03 16:21:27 -0500

    Adding improved solution

--- Arrays/problem_121. Best Time to Buy and Sell Stock.py
+++ Arrays/problem_121. Best Time to Buy and Sell Stock.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Brute Force
+        # max_profit = 0
+        # for i in range(0, len(prices)):
+        #     for j in range(i+1, len(prices)):
+        #         profit = prices[j] - prices[i]
+        #         if(profit > max_profit):
+        #             max_profit = profit
+        # return max_profit
+
+        # Optimized in one pass
+        min_element_so_far = math.inf
         max_profit = 0
-        for i in range(0, len(prices)):
-            for j in range(i+1, len(prices)):
-                profit = prices[j] - prices[i]
-                if(profit > max_profit):
-                    max_profit = profit
+        for i in range(len(prices)):
+            if prices[i] < min_element_so_far:
+                min_element_so_far = prices[i]
+            else:
+                if(prices[i] - min_element_so_far > max_profit):
+                    max_profit = prices[i] - min_element_so_far
         return max_profit
-            
-        
-            
-        
