29c06af97473ff314824176070e6f0b5192098a8
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-03 23:29:41 -0500

    Add solution

---
+++ HashTable/problem_1487. Making File Names Unique.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getFolderNames(self, names: List[str]) -> List[str]:
+
+        new_names = []
+        present_names = dict()
+        for name in names:
+            if name not in present_names:
+                present_names[name] = 1
+                new_names.append(name)
+            else:
+                mod_name = name + "(" + str(present_names[name]) + ")"
+                while(mod_name in present_names):
+                    present_names[name] += 1
+                    mod_name = name + "(" + str(present_names[name]) + ")"
+                present_names[mod_name] = 1
+                new_names.append(mod_name)
+
+        return new_names
