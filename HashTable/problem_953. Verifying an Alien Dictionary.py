e1f731404d07dd533a4bc2288f72de34dc0e8d1c
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-05 01:38:50 -0500

    Adding solution

---
+++ HashTable/problem_953. Verifying an Alien Dictionary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_chars = {}
+        for idx, val in enumerate(order):
+            order_chars[val] = idx
+
+        for i in range(len(words) - 1):
+
+            for j in range(len(words[i])):
+
+                # Account for index from 0
+                if j > len(words[i + 1]) - 1:
+                    return False
+
+                if words[i][j] != words[i+1][j]:
+                    if order_chars[words[i][j]] > order_chars[words[i+1][j]]:
+                        return False
+                    break
+        return True
