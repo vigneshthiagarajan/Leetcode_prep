9826495133b5e6ac2c612f7502ebe7df2a554d35
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-05 23:33:55 -0500

    adding solution

---
+++ Arrays/problem_724. Find Pivot Index.py
@@ -0,0 +1,19 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+
+        if len(nums) <= 2:
+            return -1
+
+        left_sum = 0
+        right_sum = sum(nums)
+
+        for i in range(0, len(nums)):
+            if(i == 0):
+                left_sum = 0
+            else:
+                left_sum += nums[i-1]
+            right_sum -= nums[i]
+            if(left_sum == right_sum):
+                return i
+
+        return -1
