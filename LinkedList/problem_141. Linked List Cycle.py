ba7aaa8bd754682590f031d31aca5f9de0ba66ce
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-08 22:59:12 -0500

    Adding solution

--- LinkedList/problem_141. Linked List Cycle.py
+++ LinkedList/problem_141. Linked List Cycle.py
@@ -6,17 +6,33 @@
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        # Using HashMaps
+        #         # Using HashMaps
 
-        # If empty linkedlist then cycle not possible
+        #         # If empty linkedlist then cycle not possible
+        #         if(head == None):
+        #             return False
+
+        #         nodes_traversed = set()
+        #         node = head
+        #         while(node.next != None):
+        #             if node in nodes_traversed:
+        #                 return True
+        #             nodes_traversed.add(node)
+        #             node = node.next
+        #         return False
+
+        # Floyds method
         if(head == None):
             return False
 
-        nodes_traversed = set()
-        node = head
-        while(node.next != None):
-            if node in nodes_traversed:
-                return True
-            nodes_traversed.add(node)
-            node = node.next
-        return False
+        slow_pointer = head
+        fast_pointer = head.next
+
+        while(slow_pointer != fast_pointer):
+            if(fast_pointer == None or fast_pointer.next == None):
+                return False
+
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return True
