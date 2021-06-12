7e599985712652ab7983b1e28ca512a3612e0a38
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-12 01:59:32 -0500

    Adding solution

--- Python/Arrays/problem_1099. Two Sum Less Than K.py
+++ Python/Arrays/problem_1099. Two Sum Less Than K.py
@@ -1,24 +1,25 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
 
-        # Brute force
-        req = -1
-        for i in range(0, len(nums)):
-            for j in range(i+1, len(nums)):
-                sum_of_two = nums[i] + nums[j]
-                if(sum_of_two > req and sum_of_two < k):
-                    req = sum_of_two
-        return req
+        #         # Brute force
+        #         req = -1
+        #         for i in range(0, len(nums)):
+        #             for j in range(i+1, len(nums)):
+        #                 sum_of_two = nums[i] + nums[j]
+        #                 if(sum_of_two > req and sum_of_two < k):
+        #                     req = sum_of_two
+        #         return req
 
         # Two pointers
         nums.sort()
         pointer1 = 0
-        pointer2 = len(nums)
-        ans = 0
-        while(pointer1 != pointer2):
+        pointer2 = len(nums)-1
+        ans = -1
+        while(pointer1 < pointer2):
             sum_val = nums[pointer1] + nums[pointer2]
-            if(sum_val > ans and sum_val < k):
-                ans = sum_val
+            if(sum_val < k):
+                if(sum_val > ans):
+                    ans = sum_val
                 pointer1 += 1
             else:
                 pointer2 -= 1
