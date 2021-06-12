d6ebe3bbeb28aa32d23eeb5655092fd3c2089046
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-12 01:28:40 -0500

    Update structure

---
+++ Java/LinkedList/problem_1474. Delete N Nodes After M Nodes of a Linked List.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNodes(ListNode head, int m, int n) {
+        ListNode current = head;
+        ListNode lastM = head; 
+        while(current != null){
+            int mCount = m, nCount = n;
+            while(current != null && mCount != 0){
+                lastM = current;
+                current = current.next;
+                mCount--;
+            }
+            
+            while(current != null && nCount != 0){
+                current = current.next;
+                nCount--;
+            }
+            lastM.next = current;
+        }
+        return head;
+    }
+}
