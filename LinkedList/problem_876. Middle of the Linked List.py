1c6494726aeb85b765c0d364664f7cf53a55be4d
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-11 23:36:43 -0500

    Adding solution

---
+++ LinkedList/problem_876. Middle of the Linked List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        length = 0
+        temp = head
+        while(temp != None):
+            temp = temp.next
+            length += 1
+        mid = math.floor(length/2)
+
+        count = 0
+        while(count < mid):
+            head = head.next
+            count += 1
+        return head
