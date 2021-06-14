277ba1517dc65a15064b8148470d930c7c3a67de
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-13 23:53:54 -0500

    cleanup

--- Java/Arrays/problem_1099. Two Sum Less Than K.java
+++ Java/Arrays/problem_1099. Two Sum Less Than K.java
@@ -2,10 +2,8 @@ import java.util.Arrays;
 class Solution {
     public int twoSumLessThanK(int[] nums, int k) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
         int pointer1 = 0;
         int pointer2 = nums.length - 1;
-        System.out.println(pointer2);
         int ans = -1;
         while(pointer1 < pointer2){
             int sum = nums[pointer1] + nums[pointer2];
