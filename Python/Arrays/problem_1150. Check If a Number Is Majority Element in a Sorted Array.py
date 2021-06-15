8decd96fd52e0ed0891b9cce43683c6e2c98a13d
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-14 22:34:21 -0500

    Betetr solution

--- Python/Arrays/problem_1150. Check If a Number Is Majority Element in a Sorted Array.py
+++ Python/Arrays/problem_1150. Check If a Number Is Majority Element in a Sorted Array.py
@@ -10,3 +10,8 @@ class Solution:
         if(after_idx - before_idx >= math.floor(len(nums)/2)):
             return True
         return False
+
+    # Binary search right most - left most
+
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+        return bisect_right(nums, target)-bisect_left(nums, target) > len(nums)//2
