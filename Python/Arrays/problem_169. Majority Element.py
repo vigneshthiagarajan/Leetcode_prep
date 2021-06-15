a437a4bffd922a1b353ae7a8ba25e3f0693e7787
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-14 22:20:32 -0500

    Solution added

---
+++ Python/Arrays/problem_169. Majority Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums_counts = collections.Counter(nums)
+        n = len(nums)
+        for key, value in nums_counts.items():
+            if value > math.floor(n/2):
+                return key
+        return -1
+        
