8307850db175693111fbf335c78cace2b6bc0538
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-16 01:39:47 -0500

    Adding alternate solution

--- Python/Arrays/problem_896. Monotonic Array.py
+++ Python/Arrays/problem_896. Monotonic Array.py
@@ -1,10 +1,21 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         
-        # Two pass
-        check = (all(nums[i] <= nums[i+1] for i in range(len(nums) - 1)) or all(nums[i] >= nums[i+1] for i in range(len(nums) - 1)))
-        return check
+        # # Two pass
+        # check = (all(nums[i] <= nums[i+1] for i in range(len(nums) - 1)) or all(nums[i] >= nums[i+1] for i in range(len(nums) - 1)))
+        # return check
     
+        # One pass
+        increasing = True
+        decreasing = True
+        
+        for i in range(len(nums)-1):
+            if(nums[i+1] < nums[i]):
+                increasing = False
+            if(nums[i+1] > nums[i]):
+                decreasing = False
+        return decreasing or increasing
+                
         
             
             
