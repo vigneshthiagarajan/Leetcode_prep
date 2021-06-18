f20de5b74f50d4723f7d4a36fd08c8ef3075e2f3
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-18 08:47:35 -0500

    Adding solution

---
+++ Python/Bit_Manipulation/problem_476. Number Complement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        to_do = num
+        bit = 1
+        while(to_do):
+            print(num, bit, to_do)
+            num = num ^ bit
+            bit = bit << 1
+            to_do = to_do >> 1
+        return num
+        
