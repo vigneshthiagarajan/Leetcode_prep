08458f9183e7ec3a9929446378f012654cd8f9ca
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-18 00:59:56 -0500

    Adding solution

---
+++ Python/Strings/problem_1694. Reformat Phone Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reformatNumber(self, number: str) -> str:
+        output_string = ""
+        number = number.replace(" ", "")
+        number = number.replace("-", "")
+        while(len(number) > 4):
+            output_string = output_string + number[:3] + "-"
+            number = number[3:]
+        if(len(number) == 4):
+            return output_string + number[:2] + "-" + number[2:]
+        elif(len(number) == 3):
+            return output_string + number
+        else:
+            return output_string + number
+        
