977d65f820803bf70bbcd075804b81263c2994b9
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-18 17:10:22 -0500

    Adding solution

---
+++ Python/Stack/problem_1441. Build an Array With Stack Operations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        list_output = []
+        i, j = 1, 0
+    
+        while i<n+1:  
+            if target[j]==i:
+                list_output.append("Push")
+                j+=1
+            else:
+                list_output.append("Push")
+                list_output.append("Pop")
+            if j==len(target):
+                break
+            i+=1
+        return list_output
+        
