b02ebdfed6fc4772cb716d5948c89e713e5b2b5d
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-18 00:31:35 -0500

    Adding solution to new problem

---
+++ Python/Arrays/problem_350. Intersection of Two Arrays II.py
@@ -0,0 +1,18 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        pointer1 = 0
+        pointer2 = 0
+        ans = []
+        while(pointer1 < len(nums1) and pointer2 < len(nums2)):
+            if(nums1[pointer1] == nums2[pointer2]):
+                ans.append(nums1[pointer1])
+                pointer1 += 1
+                pointer2 += 1
+            elif(nums1[pointer1] > nums2[pointer2]):
+                pointer2 += 1
+            else:
+                pointer1 += 1
+        return ans
+        
