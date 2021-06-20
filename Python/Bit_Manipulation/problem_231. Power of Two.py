419fa2cc750defe5f6041462ede8226ae075fa49
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-20 18:50:53 -0500

    Adding alternate solution

--- Python/Bit_Manipulation/problem_231. Power of Two.py
+++ Python/Bit_Manipulation/problem_231. Power of Two.py
@@ -1,8 +1,14 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        # Solution 1 : keep rightmost 1 bit
+        # if n == 0:
+        #     return False
+        # if n == (n & (-n)):
+        #     return True
+        # return False
+        
+        # Solution 2 : Turn off rightmost 1 bit - make it 0
         if n == 0:
-            return False
-        if n == (n & (-n)):
-            return True
-        return False
+            return 0
+        return n & (n-1) == 0
         
