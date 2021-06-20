e78a162f60fa8caa2f585aca92c369c5154018b1
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-19 23:05:41 -0500

    Adding solution

---
+++ Python/Arrays/problem_258. Add Digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        digits_list = list(map(int, str(num)))
+        sum_digits = sum(digits_list)
+        while(len(digits_list) > 1):
+            digits_list = list(map(int, str(sum_digits)))
+            sum_digits = sum(digits_list)
+        return sum_digits
