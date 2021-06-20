111eb4cef489f34110b31f117ecb0cdf6c933cda
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-20 18:19:04 -0500

    Adding solution

---
+++ Python/Bit_Manipulation/problem_338. Counting Bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        return_array = []
+        for i in range(n+1):
+            count = 0
+            while(i):
+                if(i & 1):
+                    count += 1
+                i = i >> 1
+            return_array.append(count)
+        return return_array
