0db7904716a096ce168589cdf16d396340878501
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-20 23:41:58 -0500

    Add solution

---
+++ Python/Arrays/problem_1742. Maximum Number of Balls in a Box.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        mapper = {}
+        for ball_num in range(lowLimit, highLimit + 1):
+            ball_sum_digits = sum(list(map(int, str(ball_num))))
+            if ball_sum_digits not in mapper:
+                mapper[ball_sum_digits] = 1
+            else:
+                mapper[ball_sum_digits] += 1
+        max_box = max(mapper.values())
+        return max_box            
+            
+        
