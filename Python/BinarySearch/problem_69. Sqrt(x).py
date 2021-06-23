3276b0b84f30b84667d2ef8755863197a65dd99e
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-22 23:52:53 -0500

    Adding new solution

---
+++ Python/BinarySearch/problem_69. Sqrt(x).py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if(x < 2):
+            return x
+        left = 2
+        right = x // 2
+        
+        while(left <= right):
+            pivot = left + (right - left) // 2
+            num = pivot * pivot
+            if(num == x):
+                return pivot
+            elif num > x:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+            
+        return right
+        
