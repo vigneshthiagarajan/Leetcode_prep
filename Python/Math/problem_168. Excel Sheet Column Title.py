ba2ee163946f4ee86dfad47f53c68b39a33c5c6d
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-28 00:53:46 -0500

    Removing unnecessary lines

--- Python/Math/problem_168. Excel Sheet Column Title.py
+++ Python/Math/problem_168. Excel Sheet Column Title.py
@@ -1,10 +1,9 @@
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        alphabets = list(alphabets)
+        alphabet_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         title = ""
         while(columnNumber > 0):
             columnNumber, remainder = divmod(columnNumber - 1, 26)
-            title = alphabets[remainder] + title
+            title = alphabet_char[remainder] + title
         return title
             
