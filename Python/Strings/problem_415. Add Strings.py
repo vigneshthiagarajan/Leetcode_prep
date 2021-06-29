1ef11ec90c0e8a012aeabe27e4ac684347641dab
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-06-29 14:52:47 -0500

    Adding solution

--- Python/Strings/problem_415. Add Strings.py
+++ Python/Strings/problem_415. Add Strings.py
@@ -4,12 +4,12 @@ class Solution:
         pointer1 = len(num1) - 1
         pointer2 = len(num2) - 1
         propagate = 0
-        while(pointer1 >= 0 or pointer2 >= 0):
-            if(pointer1 >= 0):
+        while pointer1 >= 0 or pointer2 >= 0:
+            if pointer1 >= 0:
                 num1_digit = int(num1[pointer1])
             else:
                 num1_digit = 0
-            if(pointer2 >= 0):
+            if pointer2 >= 0:
                 num2_digit = int(num2[pointer2])
             else:
                 num2_digit = 0
@@ -19,6 +19,6 @@ class Solution:
             return_string = str(digit_req) + return_string
             pointer1 -= 1
             pointer2 -= 1
-        if(propagate > 0):
+        if propagate > 0:
             return_string = "1" + return_string
         return return_string
