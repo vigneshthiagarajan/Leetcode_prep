32967bbcb9fcd14645e78d071a219f6966ed48f7
Vignesh Thiagarajan <vigneshthiagaraj@gmail.com>
2021-07-10 17:48:42 -0500

    Adding python code

---
+++ Python/BinarySearch/problem_35. Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        high = len(nums) - 1
+        low = 0
+        if len(nums) == 0:
+            return 0
+        while(low <= high):
+            pivot = int((low + high)/2)
+            if(nums[pivot] == target):
+                return pivot
+            elif nums[pivot] < target:
+                low = low + 1
+            else:
+                high = high - 1
+        return low
